e610fd6df7bf47f746811f02cb0c13b8333ea7fe
frnaklynd <frnaklynd@gmail.com>
2015-05-11 01:10:28 -0400

    added settings for postgresql

--- savvi_backend/savvi_backend/settings.py
+++ savvi_backend/savvi_backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'savvi_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'savvi_db',
+        'USER': 'frank',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
