3a59b774c10c85ff38fb9c4c99de17ab49a7de51
frnaklynd <frnaklynd@gmail.com>
2015-05-12 16:12:21 -0400

    organizing the models that compose the backend

---
+++ backend/savvi/apps/ideas/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class idea(models.Model):
+    #To do idea modeling by frank :D
+    
+
+class comments(models.Model):
+    #also to do
+
---
+++ backend/savvi/apps/users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class user(models.Model):
+    #to do , user modeling by mchp
+
+#maybe have a one-to-one profile model ?
+class profile(models.Model):
