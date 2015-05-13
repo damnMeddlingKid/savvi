324447250d2e26b32e95cb767a81fe558fdeea86
Franklyn <franklynd@gmail.com>
2015-05-13 02:58:12 -0400

    added a voting method to the ideas

--- backend/savvi/apps/ideas/models.py
+++ backend/savvi/apps/ideas/models.py
@@ -1,15 +1,28 @@
+from enum import Enum
 from django.db import models
 from savvi.apps.users.models import User
+
+VoteType = Enum('VoteType','UP_VOTE DOWN_VOTE')
 
 class Idea(models.Model):
     author = models.ForeignKey(User)
     content = models.CharField(max_length=300)
     upvote_count = models.IntegerField()
     downvote_count = models.IntegerField()
+
+    #this will be ok for the MVP, but it wont scale.
+    #should do it async
+    def Vote(self,vote_type):
+        if(vote_type == VoteType.UP_VOTE):
+            self.upvote_count += 1
+        elif(vote_type == VoteType.DOWN_VOTE):
+            self.downvote_count += 1
+        else:
+            raise Exception('INVALID VOTE %d' % vote_type)
     
 class Comment(models.Model):
     author = models.ForeignKey(User)
-    topic = models.ForeignKey(Idea)
+    idea = models.ForeignKey(Idea)
     content = models.TextField()    
-    
+    parent = models.ForeignKey('self',null=True,blank=True)
 
